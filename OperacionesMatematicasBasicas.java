c7239e0cc172a48e9d14c8d9736f0f97ea7496f8
mike2202th <140124553+mike2202th@users.noreply.github.com>
2026-05-22 00:00:24 -0400

    Add files via upload

---
+++ OperacionesMatematicasBasicas.java
@@ -0,0 +1,21 @@
+
+/*Realizar un programa que incluya las 4 operaciones matemáticas básicas (Suma, Resta, Multiplicación y División).
+💡 Sugerencia: Declara dos variables tipo int o double y realiza las operaciones con +, -, *, /
+*/
+
+package operacionesmatematicasbasicas;
+public class OperacionesMatematicasBasicas {
+
+    public static void main(String[] args) {
+        double x = 25;
+        double y = 13;
+        
+        System.out.println(x + y);
+        
+        System.out.println(x - y);
+        
+        System.out.println(x * y);
+        
+        System.out.println(x / y); 
+    }
+}
