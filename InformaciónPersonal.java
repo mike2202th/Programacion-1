1aaedbdf3eab942ca006b5b8ba11c543bfe73509
mike2202th <140124553+mike2202th@users.noreply.github.com>
2026-05-22 00:01:34 -0400

    Add files via upload

---
+++ InformaciónPersonal.java
@@ -0,0 +1,11 @@
+
+package informaciónpersonal;
+
+public class InformaciónPersonal {
+    public static void main(String[] args) {
+        System.out.println("Michael Alexander Cabrera Feliz");
+        
+        System.out.println("20252448@itla.edu.do");
+    }
+    
+}
