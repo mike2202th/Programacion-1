58fc0921e0fdcdffa7b3837ab0a75209509547ab
mike2202th <140124553+mike2202th@users.noreply.github.com>
2026-05-21 23:59:54 -0400

    Add files via upload

---
+++ NumerosPares.java
@@ -0,0 +1,15 @@
+
+package numerospares;
+
+public class NumerosPares {
+    public static void main(String[] args) {
+        int par = 0;
+        
+        System.out.println("Los numeros pares de 2 a 100 son: ");
+        while (par < 100){
+            par += 2;
+            System.out.println(par);
+        }
+    }
+    
+}
