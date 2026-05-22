e6d8e911f08b1041af7e3393ec7979b01c8c7b71
mike2202th <140124553+mike2202th@users.noreply.github.com>
2026-05-21 23:59:33 -0400

    Add files via upload

---
+++ NumerosImpares.java
@@ -0,0 +1,11 @@
+package numerosimpares;
+public class NumerosImpares {
+    public static void main(String[] args) {
+       System.out.println("Los numeros impares del 1 al 100 son: ");
+       
+       for (int numero = 1; numero <= 100; numero += 2){
+           System.out.println(numero);
+       }
+    }
+    
+}
